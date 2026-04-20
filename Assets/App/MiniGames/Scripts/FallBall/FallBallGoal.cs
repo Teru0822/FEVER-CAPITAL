09dfb0b247c92e8c97c4be51a8fe945cab34b909
Shinya Sato <c612221025@tokushima-u.ac.jp>
2026-04-21 06:39:28 +0900

    feat: ゴールとミス判定用のトリガースクリプトを追加

---
+++ Assets/App/MiniGames/Scripts/FallBall/FallBallGoal.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace MiniGames.FallBall
+{
+    /// <summary>
+    /// 筒（ゴール）に配置し、鉄球が中に入ったことを検知するスクリプト。
+    /// OnGoalReached() を FallBallGameManager に伝達します。
+    /// </summary>
+    [RequireComponent(typeof(Collider))]
+    public class FallBallGoal : MonoBehaviour
+    {
+        [Tooltip("このゴールが通知を送る対象の GameManager")]
+        [SerializeField] private FallBallGameManager gameManager;
+        
+        [Tooltip("鉄球オブジェクトに付けられているタグ（デフォは 'Player' など）")]
+        [SerializeField] private string ballTag = "Player";
+
+        private void OnTriggerEnter(Collider other)
+        {
+            // ぶつかってきた（中に入った）オブジェクトのタグが鉄球のものだったら
+            if (other.CompareTag(ballTag))
+            {
+                if (gameManager != null)
+                {
+                    // GameManagerの成功処理を呼び出す
+                    gameManager.OnGoalReached();
+                }
+                else
+                {
+                    Debug.LogWarning("FallBallGoal: GameManager がアタッチされていません！");
+                }
+            }
+        }
+    }
+}
